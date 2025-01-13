f837596ccd9e94064dc8b1a112bab9c20c2817c4
Rutger Boekhorst <138577231+RutgerICATT@users.noreply.github.com>
2025-01-13 13:42:14 +0100

    Add files via upload

---
+++ docs/scripts/install_kiss.ps1
@@ -0,0 +1,109 @@
+$env = "dev"
+$cert = "__$env-kiss-demo_nl.crt"
+$key = "kiss-$env-private.key"
+$namespace = "kiss-namespace"
+$kibanaHost = "kibana.dev.kiss-demo.nl"
+
+kubectl create namespace $namespace
+kubectl config current-context
+kubectl config set-context --current --namespace=$namespace
+
+# Add helm repos
+helm repo add ingress-nginx-repo https://kubernetes.github.io/ingress-nginx
+helm repo add kiss-repo-ci https://raw.githubusercontent.com/Klantinteractie-Servicesysteem/KISS-frontend/main/helm
+helm repo add kiss-elastic https://raw.githubusercontent.com/Klantinteractie-Servicesysteem/.github/main/docs/scripts/elastic
+helm repo update
+
+# Install nginx ingress
+helm upgrade --install --namespace kiss-namespace ingress-nginx-release ingress-nginx-repo/ingress-nginx `
+ --set controller.service.externalTrafficPolicy=Local --set annotations.nginx.ingress.kubernetes.io/proxy-buffer-size=16k
+
+ # create TLS secret
+kubectl create secret tls --cert $cert --key=$key wildcard-kiss-tls -n $namespace
+
+# Install elastic components
+kubectl create -f https://download.elastic.co/downloads/eck/2.9.0/crds.yaml
+kubectl apply -f https://download.elastic.co/downloads/eck/2.9.0/operator.yaml
+helm upgrade --set kibanaDomain=$kibanaHost --install kiss-elastic kiss-elastic/kiss-elastic --version 0.1.0
+
+$POSTGRES_PASSWORD = 'ChangeThis'
+$ELASTIC_PASSWORD = kubectl get secret kiss-es-elastic-user -o go-template='{{.data.elastic | base64decode }}'
+
+# Add config
+kubectl create configmap kiss-config `
+    --from-literal=AFDELINGEN_BASE_URL=$(AFDELINGEN_BASE_URL) `
+    --from-literal=AFDELINGEN_OBJECT_TYPE_URL=$(AFDELINGEN_OBJECT_TYPE_URL) `
+    --from-literal=ASPNETCORE_ENVIRONMENT=$(ASPNETCORE_ENVIRONMENT) `
+    --from-literal=CONTACTMOMENTEN_API_CLIENT_ID=$(CONTACTMOMENTEN_API_CLIENT_ID) `
+    --from-literal=CONTACTMOMENTEN_BASE_URL=$(CONTACTMOMENTEN_BASE_URL) `
+    --from-literal=ELASTIC_BASE_URL=$(ELASTIC_BASE_URL) `
+    --from-literal=ELASTIC_USERNAME=$(ELASTIC_USERNAME) `
+    --from-literal=EMAIL_ENABLE_SSL=$(EMAIL_ENABLE_SSL) `
+    --from-literal=EMAIL_HOST=$(EMAIL_HOST) `
+    --from-literal=EMAIL_PORT=$(EMAIL_PORT) `
+    --from-literal=EMAIL_USERNAME=$(EMAIL_USERNAME) `
+    --from-literal=ENTERPRISE_SEARCH_BASE_URL=$(ENTERPRISE_SEARCH_BASE_URL) `
+    --from-literal=ENTERPRISE_SEARCH_ENGINE=$(ENTERPRISE_SEARCH_ENGINE) `
+    --from-literal=FEEDBACK_EMAIL_FROM=$(FEEDBACK_EMAIL_FROM) `
+    --from-literal=FEEDBACK_EMAIL_TO=$(FEEDBACK_EMAIL_TO) `
+    --from-literal=GROEPEN_BASE_URL=$(GROEPEN_BASE_URL) `
+    --from-literal=GROEPEN_OBJECT_TYPE_URL=$(GROEPEN_OBJECT_TYPE_URL) `
+    --from-literal=HAAL_CENTRAAL_BASE_URL=$(HAAL_CENTRAAL_BASE_URL) `
+    --from-literal=INTERNE_TAAK_BASE_URL=$(INTERNE_TAAK_BASE_URL) `
+    --from-literal=INTERNE_TAAK_OBJECT_TYPE_URL=$(INTERNE_TAAK_OBJECT_TYPE_URL) `
+    --from-literal=KLANTEN_BASE_URL=$(KLANTEN_BASE_URL) `
+    --from-literal=KLANTEN_CLIENT_ID=$(KLANTEN_CLIENT_ID) `
+    --from-literal=KLANTINTERACTIES_BASE_URL=$(KLANTINTERACTIES_BASE_URL) `
+    --from-literal=KVK_BASE_URL=$(KVK_BASE_URL) `
+    --from-literal=MEDEWERKER_OBJECTEN_BASE_URL=$(MEDEWERKER_OBJECTEN_BASE_URL) `
+    --from-literal=MEDEWERKER_OBJECTTYPES_BASE_URL=$(MEDEWERKER_OBJECTTYPES_BASE_URL) `
+    --from-literal=OIDC_AUTHORITY=$(OIDC_AUTHORITY) `
+    --from-literal=OIDC_CLIENT_ID=$(OIDC_CLIENT_ID) `
+    --from-literal=OIDC_MEDEWERKER_IDENTIFICATIE_CLAIM=$(OIDC_MEDEWERKER_IDENTIFICATIE_CLAIM) `
+    --from-literal=OIDC_MEDEWERKER_IDENTIFICATIE_TRUNCATE=$(OIDC_MEDEWERKER_IDENTIFICATIE_TRUNCATE) `
+    --from-literal=ORGANISATIE_IDS=$(ORGANISATIE_IDS) `
+    --from-literal=POSTGRES_DB=$(POSTGRES_DB) `
+    --from-literal=POSTGRES_HOST=$(POSTGRES_HOST) `
+    --from-literal=POSTGRES_USER=$(POSTGRES_USER) `
+    --from-literal=SDG_BASE_URL=$(SDG_BASE_URL) `
+    --from-literal=SDG_OBJECTEN_BASE_URL=$(SDG_OBJECTEN_BASE_URL) `
+    --from-literal=SDG_OBJECT_TYPE_URL=$(SDG_OBJECT_TYPE_URL) `
+    --from-literal=VAC_OBJECTEN_BASE_URL=$(VAC_OBJECTEN_BASE_URL) `
+    --from-literal=VAC_OBJECTTYPES_BASE_URL=$(VAC_OBJECTTYPES_BASE_URL) `
+    --from-literal=ZAAKSYSTEEM__0__API_CLIENT_ID=$(ZAAKSYSTEEM__0__API_CLIENT_ID) `
+    --from-literal=ZAAKSYSTEEM__0__BASE_URL=$(ZAAKSYSTEEM__0__BASE_URL) `
+    --from-literal=ZAAKSYSTEEM__0__DEEPLINK_URL=$(ZAAKSYSTEEM__0__DEEPLINK_URL) `
+    --from-literal=ZAAKSYSTEEM__0__DEEPLINK_PROPERTY=$(ZAAKSYSTEEM__0__DEEPLINK_PROPERTY) `
+    --from-literal=ZAAKSYSTEEM__0__NIETNATUURLIJKPERSOONIDENTIFIER=$(ZAAKSYSTEEM__0__NIETNATUURLIJKPERSOONIDENTIFIER) `
+    --dry-run=client -o yaml | kubectl apply -f -
+# Add secrets
+kubectl create secret generic kiss-secrets `
+    --from-literal=AFDELINGEN_TOKEN=$(AFDELINGEN_TOKEN) `
+    --from-literal=CONTACTMOMENTEN_API_KEY=$(CONTACTMOMENTEN_API_KEY) `
+    --from-literal=ELASTIC_PASSWORD=$ELASTIC_PASSWORD `
+    --from-literal=EMAIL_PASSWORD=$(EMAIL_PASSWORD) `
+    --from-literal=ENTERPRISE_SEARCH_PRIVATE_API_KEY=$(ENTERPRISE_SEARCH_PRIVATE_API_KEY) `
+    --from-literal=ENTERPRISE_SEARCH_PUBLIC_API_KEY=$(ENTERPRISE_SEARCH_PUBLIC_API_KEY) `
+    --from-literal=GROEPEN_TOKEN=$(GROEPEN_TOKEN) `
+    --from-literal=HAAL_CENTRAAL_API_KEY=$(HAAL_CENTRAAL_API_KEY) `
+    --from-literal=INTERNE_TAAK_TOKEN=$(INTERNE_TAAK_TOKEN) `
+    --from-literal=KLANTEN_CLIENT_SECRET=$(KLANTEN_CLIENT_SECRET) `
+    --from-literal=KLANTINTERACTIES_TOKEN=$(KLANTINTERACTIES_TOKEN) `
+    --from-literal=KVK_API_KEY=$(KVK_API_KEY) `
+    --from-literal=MEDEWERKER_OBJECTEN_TOKEN=$(MEDEWERKER_OBJECTEN_TOKEN) `
+    --from-literal=MEDEWERKER_OBJECTTYPES_TOKEN=$(MEDEWERKER_OBJECTTYPES_TOKEN) `
+    --from-literal=OIDC_CLIENT_SECRET=$(OIDC_CLIENT_SECRET) `
+    --from-literal=POSTGRES_PASSWORD=$POSTGRES_PASSWORD `
+    --from-literal=SDG_OBJECTEN_TOKEN=$(SDG_OBJECTEN_TOKEN) `
+    --from-literal=VAC_OBJECTEN_TOKEN=$(VAC_OBJECTEN_TOKEN) `
+    --from-literal=VAC_OBJECTTYPES_TOKEN=$(VAC_OBJECTTYPES_TOKEN) `
+    --from-literal=ZAAKSYSTEEM__0__API_KEY=$(ZAAKSYSTEEM__0__API_KEY) `
+    --dry-run=client -o yaml | kubectl apply -f -
+
+helm upgrade --install kiss-ci-release kiss-repo-ci/kiss-frontend --version 1.0.51 `
+    --values [local dir]\kiss.template.yaml `
+    --set image.tag='latest' `
+    --set global.postgresql.auth.username='kiss.bff' `
+    --set global.postgresql.auth.password=$POSTGRES_PASSWORD `
+    --set global.postgresql.auth.database='kiss.bff' `
+    --set global.postgresql.auth.postgresPassword=$POSTGRES_PASSWORD
---
+++ profile/README.md
@@ -0,0 +1,50 @@
+
+# Klantinteractie Servicesysteem (KISS)
+
+Het Klantinteractie Servicesysteem (KISS) is een applicatie waarmee Klantcontactmedewerkers (KCM) optimaal worden ondersteund in hun werk: het informeren en helpen van burgers en ondernemers die contact opnemen met de gemeente. Doel van KISS is om alle informatie die een KCM nodig heeft voor die taak, zoveel mogelijk binnen KISS zelf te ontsluiten. Het is ontwikkeld als een [Common Ground component](https://commonground.nl/groups/view/07b18e2c-471b-4874-80b5-020dfe4ad52e/team-kiss) in opdracht van de gemeente Utrecht en Dimpact.
+
+
+## Roadmap en doorontwikkeling
+
+KISS Fase 1 is in september 2023 opgeleverd. In deze [webinar](https://vimeo.com/886406377?share=copy) worden de functies van KISS gedemonstreerd.  
+De doorontwikkeling van KISS ligt bij de gemeentelijke samenwerking. Neem bij interesse voor deelname aan de samenwerking of voor overige vragen contact op met Moniek Koerhuis via m.koerhuis@enschede.nl of met Hans Peters via hans.peters@haarlemmermeer.nl.
+
+
+## Documentatie
+
+- [Documentatie](https://kiss-klantinteractie-servicesysteem.readthedocs.io/):
+  - [Installatie handleiding](https://kiss-klantinteractie-servicesysteem.readthedocs.io/en/latest/INSTALLATION/)
+  - [Configuratie](https://kiss-klantinteractie-servicesysteem.readthedocs.io/en/latest/CONFIGURATIE/)
+  - [Handleiding beheer](https://kiss-klantinteractie-servicesysteem.readthedocs.io/en/latest/MANUAL/)
+  - [Ontwerpbeslissingen](https://kiss-klantinteractie-servicesysteem.readthedocs.io/en/latest/DECISION-RECORD/)
+  - [Definition of Done](https://kiss-klantinteractie-servicesysteem.readthedocs.io/en/latest/DEFINITIONOFDONE/)
+  - [Security](https://kiss-klantinteractie-servicesysteem.readthedocs.io/en/latest/SECURITY/)
+
+
+## Waaruit bestaat KISS-frontend
+
+De KISS-applicatie bestaat uit een User Interface (Vue) op een Backend for frontend (.Net + PostgreSQL).  Kennisbronnen worden ontsloten m.b.v, een Search Index (Elastic Search). 
+
+
+## API's 
+De KISS-applicatie maakt gebruik van enkele landelijke basisregistraties. Daarnaast implementeert KISS verschillende standaarden uit het domein van Common Ground, die zijn ontwikkeld onder de vlag van VNG, en waarvan sommige inmiddels ook [door VNG gestandaardiseerd zijn](https://www.gemmaonline.nl/index.php/Ontwikkelagenda_API-standaarden).  
+
+- [KvK apis](https://developers.kvk.nl/documentation/zoeken-api)
+- [Haal Centraal BRP Personen bevragen](https://brp-api.github.io/Haal-Centraal-BRP-bevragen/) 
+- [ZGW Zaken](https://openzaak.prod.kiss-demo.nl/zaken/api/v1/schema/) 
+- [ZGW Catalogi](https://openzaak.prod.kiss-demo.nl/catalogi/api/v1/schema/) 
+- [ZGW Documenten](https://openzaak.prod.kiss-demo.nl/documenten/api/v1/schema/)
+- [Objecttype API](https://github.com/maykinmedia/objecttypes-api/) voor Afdeling, Groep, Medewerker, VAC, PDC en Contactverzoek als interne taak ([deze worden momenteel gereviewed, zie ook de Community concepts binnen Objecttypes, bij Open Objecten](https://github.com/open-objecten/objecttypes/tree/main/community-concepts)
+  - Het PDC-object is gebaseerd op de API van [de SDG-invoervoorziening (versie 1.3.0)](https://redocly.github.io/redoc/?url=https://raw.githubusercontent.com/maykinmedia/sdg-invoervoorziening/1.3.0/src/openapi.yaml&nocors)
+- [Objecten API](https://github.com/maykinmedia/objects-api)
+- [Klanten API](https://open-klant.prod.kiss-demo.nl/klanten/api/v1/schema/), zoals geïmplementeerd in v0.1 van [Open Klant](https://github.com/maykinmedia/open-klant); waarbij we zijn uitgegaan van de standaard (dus niet de afwijkingen op de standaard, die zijn gemarkeerd is de specificatie)
+- [Contactmomenten API](https://open-klant.prod.kiss-demo.nl/contactmomenten/api/v1/schema/), zoals geïmplementeerd in v0.1 van [Open Klant](https://github.com/maykinmedia/open-klant); waarbij we zijn uitgegaan van de standaard (dus niet de afwijkingen op de standaard, die zijn gemarkeerd is de specificatie)
+
+Daar waar de API-standaarden ontoereikend zijn of ontbreken breiden we de API's uit volgens het vastgesteld ontwerpprincipe "afwijking van de standaard" VNG/Dimpact 16 juni 2023 ([zie: 20230616-PodiumD-flow-afwijkingen-standaarden.pdf](https://github.com/Klantinteractie-Servicesysteem/.github/blob/main/docs/files/20230616-PodiumD-flow-afwijkingen-standaarden.pdf)).
+
+
+## Bouw
+KISS wordt ontwikkeld door ICATT menselijk digitaal, in opdracht van Utrecht en Dimpact. 
+
+## Licentie
+[European Union Public License](https://opensource.org/licenses/EUPL-1.2)
